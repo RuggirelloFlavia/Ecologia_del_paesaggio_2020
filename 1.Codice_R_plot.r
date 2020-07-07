146ce9539bbe180e1bfaf66114aa1e3ab59d24c8
RuggirelloFlavia <67957069+RuggirelloFlavia@users.noreply.github.com>
2020-07-07 13:49:13 +0200

    Update 1.Codice_R_plot.r

--- 1.Codice_R_plot.r
+++ 1.Codice_R_plot.r
@@ -1,43 +1,43 @@
-#installiamo i pacchetti che ci servono
+FR#installiamo i pacchetti che ci servono
 
 install.packages("sp")
 library(sp) 
 
-# “data” serve per chiamare i dati contenuti nella libreria
+FR# “data” serve per chiamare i dati contenuti nella libreria
 data("meuse")
 
-#”meuse” vediamo il contenuto
+FR#”meuse” vediamo il contenuto
 meuse
 
-#vediamo solo le prime 6 righe della tabella
+FR#vediamo solo le prime 6 righe della tabella
 head(meuse)
 
-#”names “ serve per vedere i nomi delle variabili contenute all'interno della tabella
+FR#”names “ serve per vedere i nomi delle variabili contenute all'interno della tabella
 names(meuse)
 
-#summary è una funzione generica utilizzata per avere una sintesi dei risultati di varie altre funzioni
+FR#summary è una funzione generica utilizzata per avere una sintesi dei risultati di varie altre funzioni
 summary(meuse)
 
-#”pairs” crea un grafico che mette in relazione le variabili dei nostri dati
+FR#”pairs” crea un grafico che mette in relazione le variabili dei nostri dati
 pairs(meuse)
 
-#il simbolo messo prima di cadmium ci fa mettere in correlazione solamente le variabili da noi specificate - impostando i dati uguali a meuse
+FR#il simbolo messo prima di cadmium ci fa mettere in correlazione solamente le variabili da noi specificate - impostando i dati uguali a meuse
 pairs(~cadmium+copper+lead,data=meuse)
 
 pairs(~cadmium+copper+lead+zinc,data = meuse)
 
-#metodo alternativo se siamo interessati a determinate righe
+FR#metodo alternativo se siamo interessati a determinate righe
 pairs(meuse[,3:6])
 
-# col="red", cambia il colore del grafico
+FR# col="red" cambia il colore del grafico
 pairs(meuse[,3:6],col="red")
 
-# “main” lo utilizziamo per dare un titolo al grafico
+FR# “main” lo utilizziamo per dare un titolo al grafico
 pairs(meuse[,3:6],col="purple",pch=19,cex=3, main="Elementi presenti")
 
 pairs(~cadmium+copper+lead+zinc+elev,data = meuse) 
 
-#prendiamo delle funzioni esterne;
+FR#prendiamo delle funzioni esterne
 panel.correlations <- function(x, y, digits=1, prefix="", cex.cor)
 {
   usr <- par("usr"); on.exit(par(usr))
@@ -81,22 +81,22 @@ panel.histograms <- function(x, ...)
 
 pairs(~cadmium+copper+lead+zinc+elev,data = meuse) 
 
-#una volte inviate le tre funzioni esterne potremo creare dei grafici diversi ed esteticamente più belli
+FR#una volte inviate le tre funzioni esterne potremo creare dei grafici diversi ed esteticamente più belli
 pairs(meuse[,3:6],lower.panel = panel.correlations,upper.panel = panel.smoothing,diag.panel = panel.histograms)
 
-#exercise: varia il pannello superiore ed inferiore invertendoli
+FR#esercizio: varia il pannello superiore ed inferiore invertendoli
 pairs(meuse[,3:6],lower.panel = panel.smoothing,upper.panel = panel.correlations,diag.panel = panel.histograms)
 
 library(sp)
 data("meuse")
- #testa di meuse
+FR#testa di meuse
 head(meuse) 
 
-#creiamo un grafico prendendo le colonne di interesse usando il comando “meuse$”
+FR#creiamo un grafico prendendo le colonne di interesse usando il comando “meuse$”
 plot(meuse$cadmium,meuse$copper, main="cadmio e rame correlati")
 
-#mette in evidenza il nostro pacchetto di dati 
+FR#mette in evidenza il nostro pacchetto di dati 
 attach(meuse)
 
-# messo in evidenza meuse non dobbiamo  scriverlo ogni volta 
+FR# messo in evidenza meuse non dobbiamo  scriverlo ogni volta 
 plot(cadmium,copper, main="Cadmio e Rame",pch=24,col="purple",xlab="Cadmio",ylab="Rame",cex=2)
